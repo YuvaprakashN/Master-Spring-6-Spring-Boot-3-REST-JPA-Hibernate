80d12867194a25b665a0f919efe717f33894ce4b
YuvaprakashN <yuvaprakashnv1999@gmail.com>
2025-01-10 17:56:27 +0530

    Spring bean web scope

--- src/main/java/com/eazybytes/eazyschool/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/eazyschool/service/ContactService.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.eazyschool.service;
 
 import com.eazybytes.eazyschool.eazyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.RequestScope;
@@ -12,6 +13,7 @@ import org.springframework.web.context.annotation.SessionScope;
 //@RequestScope
 //@SessionScope
 @ApplicationScope
+//@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class ContactService {
 
     private int counter;
