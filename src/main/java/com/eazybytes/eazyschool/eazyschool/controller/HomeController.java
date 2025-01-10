6154c698024ad8feb9b1a98932ffb0dbab44c0ed
YuvaprakashN <yuvaprakashnv1999@gmail.com>
2025-01-10 14:42:34 +0530

    Add home page

---
+++ src/main/java/com/eazybytes/eazyschool/eazyschool/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.eazybytes.eazyschool.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+
+        return "home.html";
+    }
+}
