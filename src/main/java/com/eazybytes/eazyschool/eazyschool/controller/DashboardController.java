45f476b5048290d374a43ebff6d4d5a30df5cd13
YuvaprakashN <yuvaprakashnv1999@gmail.com>
2025-01-11 08:55:35 +0530

    Spring security - Error message

--- src/main/java/com/eazybytes/eazyschool/eazyschool/controller/DashboardController.java
+++ src/main/java/com/eazybytes/eazyschool/eazyschool/controller/DashboardController.java
@@ -13,6 +13,7 @@ public class DashboardController {
     public String displayDashboard(Model model, Authentication authentication) {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+       // throw new RuntimeException("Bug");
         return "dashboard.html";
     }
 }
