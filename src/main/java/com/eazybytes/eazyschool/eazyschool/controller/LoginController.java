d076e33e7f07ef22a189d3440be2ee8c984174c4
YuvaprakashN <yuvaprakashnv1999@gmail.com>
2025-01-11 09:48:17 +0530

    Spring security - Implemented CSRF protection

--- src/main/java/com/eazybytes/eazyschool/eazyschool/controller/LoginController.java
+++ src/main/java/com/eazybytes/eazyschool/eazyschool/controller/LoginController.java
@@ -1,18 +1,22 @@
 package com.eazybytes.eazyschool.eazyschool.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    @PostMapping("/login")
+  //  @GetMapping("/login")
+   // @PostMapping("/login")
+    @RequestMapping(value = "/login",method = {RequestMethod.GET, RequestMethod.POST})
     public String loginPage(@RequestParam(required = false, value = "error") String error, @RequestParam(required = false, value = "logout") String logout, Model model) {
 
         String errMsg = null;
@@ -23,5 +27,14 @@ public class LoginController {
 
         model.addAttribute("errorMessge", errMsg);
         return "login.html";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
     }
 }
